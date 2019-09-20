7bdd7e3876797667a90e2aa7dfc0da2a827af79d
Ian Rose <ian.rose@lacity.org>
2019-09-20 10:31:07 -0700

    Publish git-completion.

--- image/custom.sh
+++ image/custom.sh
@@ -2,10 +2,11 @@
 
 
 # enable bash completion in interactive shells
-# enable git prompt
 if ! shopt -oq posix; then
-  if [ -f /etc/bash_completion.d/git-prompt ]; then
-    . /etc/bash_completion.d/git-prompt
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
   fi
 fi
 
